6bdf465a666433a53e77e4d9f471b95529ddf0c5
sbrinton19 <sbrinton@purdue.edu>
2014-08-03 17:38:37 -0400

    Player GUI basic version finished
    Doesn't have a lot of automated catches or handles for ability counts,
    allowed abilities, etc. But supports all required functionality

--- src/XCOM/Human.java
+++ src/XCOM/Human.java
@@ -1,5 +1,7 @@
 package XCOM;
 
+import java.util.ArrayList;
+
 public class Human {
 	public static final String[] classes = { "Heavy", "Assault", "Support", "Sniper" };
 	public static final String[] nationalities = { "Egyptian", "Nigerian", "South African", "Australian",
@@ -44,10 +46,207 @@ public class Human {
 	//Max 5
 	public byte[] medalList;
 	public boolean mec;
+	public boolean gene;
 	public boolean psi;
 	public boolean sex;
 	public Human(){
 		abilityList = new byte[16];
+		for(int i = 0; i < abilityList.length; i++)
+			abilityList[i] = -1;
 		medalList = new byte[5];
+		for(int i = 0; i < medalList.length; i++)
+			medalList[i] = -1;
+	}
+	
+	public static byte getAbilityID(String name){
+		for(int i = 0; i < abilities.length;i++){
+			if(name.equals(abilities[i]))
+				return (byte) i;
+		}
+		for(int i = 0; i < mecAbilities.length;i++){
+			if(name.equals(mecAbilities[i]))
+				return (byte) (i + abilities.length);
+		}
+		for(int i = 0; i < psionicAbilities.length; i++){
+			if(name.equals(psionicAbilities[i]))
+				return (byte) (i + abilities.length+mecAbilities.length);
+		}
+		for(int i = 0; i < geneAbilities.length; i++){
+			if(name.equals(geneAbilities[i]))
+				return (byte) (i + abilities.length+mecAbilities.length+psionicAbilities.length);
+		}
+		return -1;
+	}
+	
+	public static byte getMedalID(String medal){
+		for(int i = 0; i < medals.length; i++){
+			if(medal.equals(medals[i]))
+				return (byte) i;
+		}
+		return -1;
+	}
+	
+	public static String getAbilityByID(byte id){
+		if(id < 0)
+			return "";
+		if(id < abilities.length)
+			return abilities[id];
+		id -= abilities.length;
+		if (id < mecAbilities.length)
+			return mecAbilities[id];
+		id -= mecAbilities.length;
+		if(id < psionicAbilities.length)
+			return psionicAbilities[id];
+		id -= psionicAbilities.length;
+		if(id < geneAbilities.length)
+			return geneAbilities[id];
+		return "";
+	}
+	
+	public String getRank(){
+		if(xp < 90){
+			return ranks[0];
+		}
+		else if(xp < 480){
+			return ranks[1];
+		}
+		else if(xp < 700){
+			return ranks[2];
+		}
+		else if(xp < 925){
+			return ranks[3];
+		}
+		else if(xp < 1380){
+			return ranks[4];
+		}
+		else if(xp < 1840){
+			return ranks[5];
+		}
+		else if(xp < 2550){
+			return ranks[6];
+		}
+		else
+			return ranks[7];
+	}
+	
+	public String getAbilitiesString(){
+		StringBuilder abilities = new StringBuilder("");
+		for(int i = 0; i < abilityList.length; i++){
+			if(i != 0 && abilityList[i]!=-1)
+				abilities.append(", ");
+			abilities.append(getAbilityByID(abilityList[i]));
+			
+			if(i%4 == 0 && i != 0)
+				abilities.append("<br>");
+		}
+		abilities.append("</html>");
+		return abilities.toString();
+	}
+	
+	public String getMedalsString(){
+		StringBuilder medals = new StringBuilder("");
+		for(int i = 0; i < medalList.length; i++){
+			if(i != 0 && medalList[i] !=-1)
+				medals.append(", ");
+			try{
+				medals.append(Human.medals[medalList[i]]);
+			}catch(ArrayIndexOutOfBoundsException e){
+				//Whatever
+			}
+			
+		}
+		return medals.toString();
+	}
+	
+	public String[] getPossibleAbilities(){
+		ArrayList<String> possible = new ArrayList<String>();
+		
+		for(int i = 0; i < Human.abilities.length; i++)
+			possible.add(Human.abilities[i]);
+		
+		if(mec){
+			for(int i = 0; i < Human.mecAbilities.length; i++)
+				possible.add(Human.mecAbilities[i]);
+		}
+		else if(gene){
+			for(int i = 0; i < Human.geneAbilities.length; i++)
+				possible.add(Human.geneAbilities[i]);
+		}
+		if(psi){
+			for(int i = 0; i < Human.psionicAbilities.length; i++)
+				possible.add(Human.psionicAbilities[i]);
+		}
+		Object[] temp = possible.toArray();
+		String[] result = new String[temp.length];
+		for(int i = 0; i < result.length;i++)
+			result[i] = (String) temp[i];
+		return result;
+	}
+	public boolean hasAbility(String ability){
+		byte abilityID = Human.getAbilityID(ability);
+		if(abilityID == -1)
+			return false;
+		for(int i = 0; i< abilityList.length; i++){
+			if(abilityList[i] == abilityID)
+				return true;
+		}
+		return false;
+	}
+	
+	public boolean hasMedal(String medal){
+		byte medalID = Human.getMedalID(medal);
+		for(int i = 0; i < medalList.length; i++){
+			if(medalList[i] == medalID)
+				return true;
+		}
+		return false;
+	}
+
+	public void addAbility(String ability){
+		byte abilityID = Human.getAbilityID(ability);
+		if(abilityID == -1)
+			return;
+		for(int i = 0; i< abilityList.length; i++){
+			if(abilityList[i] == -1){
+				abilityList[i] = abilityID;
+				return;
+			}
+		}
+	}
+	
+	public void addMedal(String medal){
+		byte medalID = Human.getMedalID(medal);
+		if(medalID == -1)
+			return;
+		for(int i =0; i< medalList.length;i++){
+			if(medalList[i] == -1){
+				medalList[i] = medalID;
+				return;
+			}
+		}
+	}
+	
+	public void removeMedal(String medal){
+		byte medalID = Human.getMedalID(medal);
+		if(medalID == -1)
+			return;
+		for(int i =0; i< medalList.length;i++){
+			if(medalList[i] == medalID){
+				medalList[i] = -1;
+				return;
+			}
+		}
+	}
+	
+	public void removeAbility(String ability){
+		byte abilityID = Human.getAbilityID(ability);
+		if(abilityID == -1)
+			return;
+		for(int i = 0; i< abilityList.length; i++){
+			if(abilityList[i] == abilityID){
+				abilityList[i] = -1;
+				return;
+			}
+		}
 	}
 }
--- src/XCOM/PlayerGUI.java
+++ src/XCOM/PlayerGUI.java
@@ -10,12 +10,14 @@ import java.awt.event.MouseListener;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.util.ArrayList;
 
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.JScrollPane;
@@ -33,6 +35,7 @@ public class PlayerGUI extends GUITemplate implements MouseListener {
 	private JButton saveHuman;
 	private JLabel humansLabel;
 	private JComboBox<String> humans;
+	private ArrayList<Human> humansList = new ArrayList<Human>();
 	
 	private ScrollPanel humanForm;
 	private JScrollPane humanPane;
@@ -69,7 +72,11 @@ public class PlayerGUI extends GUITemplate implements MouseListener {
 	private JRadioButton notPsi;
 	private ButtonGroup psion;
 	private JLabel expLabel;
+	private JLabel expAddFieldLabel;
 	private TextField exp;
+
+	private boolean changes;
+	private int humeIndex = 0;
 	
 	public PlayerGUI(){
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
@@ -84,11 +91,14 @@ public class PlayerGUI extends GUITemplate implements MouseListener {
 		deleteHuman = new JButton("Delete Human");
 		deleteHuman.addActionListener(this);
 		deleteHuman.setToolTipText("Deletes currently selected human");
+		deleteHuman.setEnabled(false);
 		saveHuman = new JButton("Save Human");
 		saveHuman.addActionListener(this);
 		saveHuman.setToolTipText("Saves currently selected human");
+		saveHuman.setEnabled(false);
 		humansLabel = new JLabel("Human Select");
 		humans = new JComboBox<String>();
+		humans.addItem("<New Human>");
 		humans.addActionListener(this);
 		
 		//HUMAN FORM
@@ -139,6 +149,7 @@ public class PlayerGUI extends GUITemplate implements MouseListener {
 		psion.add(psi);
 		psion.add(notPsi);
 		expLabel = new JLabel("XP Total: ");
+		expAddFieldLabel = new JLabel("Add XP to Total");
 		exp = new TextField("");
 		//Construct Human Form
 		GridBagConstraints c = new GridBagConstraints();
@@ -189,6 +200,7 @@ public class PlayerGUI extends GUITemplate implements MouseListener {
 		c.gridx = 0;
 		c.gridwidth = 4;
 		humanForm.add(abilitiesLabel, c);
+		
 		c.gridy = 6;
 		humanForm.add(medals,c);
 		c.gridwidth = 1;
@@ -211,6 +223,8 @@ public class PlayerGUI extends GUITemplate implements MouseListener {
 		c.gridy = 9;
 		humanForm.add(expLabel,c);
 		c.gridx = 1;
+		humanForm.add(expAddFieldLabel,c);
+		c.gridx = 2;
 		humanForm.add(exp, c);
 		
 		humanPane = new JScrollPane(humanForm);
@@ -251,20 +265,369 @@ public class PlayerGUI extends GUITemplate implements MouseListener {
 	
 	@Override
 	protected void save(ObjectOutputStream out) throws IOException {
-		// TODO Auto-generated method stub
-
+		out.writeInt(humansList.size());
+		for(int i = 0; i < humansList.size();i++){
+			Human human = humansList.get(i);
+			out.writeInt(human.name.length());
+			out.writeChars(human.name);
+			out.writeInt(human.owner.length());
+			out.writeChars(human.owner);
+			out.writeInt(human.xp);
+			out.writeInt(human.aim);
+			out.writeInt(human.will);
+			byte[] temp = {human.health, human.movement, human.charClass, human.nationality };
+			out.write(temp);
+			out.write(human.abilityList);
+			out.write(human.medalList);
+			out.writeBoolean(human.mec);
+			out.writeBoolean(human.gene);
+			out.writeBoolean(human.psi);
+			out.writeBoolean(human.sex);
+		}
+		changes = false;
 	}
 
 	@Override
 	protected void load(ObjectInputStream in) throws IOException {
-		// TODO Auto-generated method stub
-
+		if(changes){
+			int ret = JOptionPane.showConfirmDialog(this, "Do you want to save the changes to this item before loading?");
+			if(ret == 2)
+				return;
+			if(ret == 0)
+				saveDialog();
+		}
+		int count = in.readInt();
+		humansList.clear();
+		
+		humans.removeAllItems();
+		humans.addItem("<New Human>");
+		humans.setSelectedIndex(0);
+		for(int i = 0; i < count; i++){
+			Human construct = new Human();
+			int nameLength = in.readInt();
+			StringBuilder build = new StringBuilder();
+			for(int j = 0; j < nameLength; j++)
+				build.append(in.readChar());
+			construct.name = build.toString();
+			int ownerLength = in.readInt();
+			build = new StringBuilder();
+			for(int j = 0; j < ownerLength; j++)
+				build.append(in.readChar());
+			construct.owner = build.toString();
+			construct.xp = in.readInt();
+			construct.aim = in.readInt();
+			construct.will = in.readInt();
+			construct.health = in.readByte();
+			construct.movement = in.readByte();
+			construct.charClass = in.readByte();
+			construct.nationality = in.readByte();
+			in.read(construct.abilityList);
+			in.read(construct.medalList);
+			construct.mec = in.readBoolean();
+			construct.gene = in.readBoolean();
+			construct.psi = in.readBoolean();
+			construct.sex = in.readBoolean();
+			humansList.add(construct);
+			humans.addItem(construct.name);
+		}
 	}
 
 	@Override
 	protected void subActionPerformed(ActionEvent arg0) {
-		// TODO Auto-generated method stub
-
+		if(arg0.getSource() == saveHuman){
+			StringBuilder sb = new StringBuilder("");
+			if(health.getText().trim().equals("")){
+				sb.append("Health is a required field\n");
+			}
+			else{
+				try{
+					Integer.parseInt(health.getText());
+				}
+				catch(NumberFormatException e){
+					sb.append("Health must be an integer\n");
+				}
+			}
+			if(aim.getText().trim().equals("")){
+				sb.append("Aim is a required field\n");
+			}
+			else{
+				try{
+					Integer.parseInt(aim.getText());
+				}
+				catch(NumberFormatException e){
+					sb.append("Aim must be an integer\n");
+				}
+			}
+			if(will.getText().trim().equals("")){
+				sb.append("Will is a required field\n");
+			}
+			else{
+				try{
+					Integer.parseInt(will.getText());
+				}
+				catch(NumberFormatException e){
+					sb.append("Will must be an integer\n");
+				}
+			}
+			if(movement.getText().trim().equals("")){
+				sb.append("Movement is a required field\n");
+			}
+			else{
+				try{
+					Integer.parseInt(movement.getText());
+				}
+				catch(NumberFormatException e){
+					sb.append("Movement must be an integer\n");
+				}
+			}
+			if(!mech.isSelected() && !gene.isSelected() && !norm.isSelected()){
+				sb.append("Modification is a required field\n");
+			}
+			if(!psi.isSelected() && !notPsi.isSelected()){
+				sb.append("Psionic Status is a required field\n");
+			}
+			if(exp.getText().trim().equals("")){
+				sb.append("Experience is a required field (use 0)\n");
+			}
+			else{
+				try{
+					Integer.parseInt(movement.getText());
+				}
+				catch(NumberFormatException e){
+					sb.append("Experience must be an integer\n");
+				}
+			}
+			if(!sb.toString().equals("")){
+				//We have errors
+				JOptionPane.showMessageDialog(this, sb.toString(),"Cannot Save", JOptionPane.WARNING_MESSAGE);
+				return;
+			}
+			//We have no errors time to save a human
+			Human modify = humansList.get(humeIndex);
+			modify.xp += Integer.parseInt(exp.getText());
+			modify.aim = Integer.parseInt(aim.getText());
+			modify.health = Byte.parseByte(health.getText());
+			modify.will = Integer.parseInt(will.getText());
+			modify.charClass = (byte) className.getSelectedIndex();
+			modify.movement = Byte.parseByte(movement.getText());
+			modify.mec = mech.isSelected();
+			modify.gene = gene.isSelected();
+			modify.psi = psi.isSelected();
+			humansList.remove(humeIndex);
+			humansList.add(humeIndex, modify);
+			changes = true;
+			humans.setSelectedItem(modify.name);
+		}
+		else if(arg0.getSource() == addHuman){
+			StringBuilder sb = new StringBuilder("");
+			if(name.getText().trim().equals("")){
+				sb.append("Name is a required field\n");
+			}
+			if(owner.getText().trim().equals("")){
+				sb.append("Owner is a required field\n");
+			}
+			if(!male.isSelected() && !female.isSelected()){
+				sb.append("Sex is a required field\n");
+			}
+			if(health.getText().trim().equals("")){
+				sb.append("Health is a required field\n");
+			}
+			else{
+				try{
+					Integer.parseInt(health.getText());
+				}
+				catch(NumberFormatException e){
+					sb.append("Health must be an integer\n");
+				}
+			}
+			if(aim.getText().trim().equals("")){
+				sb.append("Aim is a required field\n");
+			}
+			else{
+				try{
+					Integer.parseInt(aim.getText());
+				}
+				catch(NumberFormatException e){
+					sb.append("Aim must be an integer\n");
+				}
+			}
+			if(will.getText().trim().equals("")){
+				sb.append("Will is a required field\n");
+			}
+			else{
+				try{
+					Integer.parseInt(will.getText());
+				}
+				catch(NumberFormatException e){
+					sb.append("Will must be an integer\n");
+				}
+			}
+			if(movement.getText().trim().equals("")){
+				sb.append("Movement is a required field\n");
+			}
+			else{
+				try{
+					Integer.parseInt(movement.getText());
+				}
+				catch(NumberFormatException e){
+					sb.append("Movement must be an integer\n");
+				}
+			}
+			if(!mech.isSelected() && !gene.isSelected() && !norm.isSelected()){
+				sb.append("Modification is a required field\n");
+			}
+			if(!psi.isSelected() && !notPsi.isSelected()){
+				sb.append("Psionic Status is a required field\n");
+			}
+			if(exp.getText().trim().equals("")){
+				sb.append("Experience is a required field (use 0)\n");
+			}
+			else{
+				try{
+					Integer.parseInt(movement.getText());
+				}
+				catch(NumberFormatException e){
+					sb.append("Experience must be an integer\n");
+				}
+			}
+			if(!sb.toString().equals("")){
+				//We have errors
+				JOptionPane.showMessageDialog(this, sb.toString(),"Cannot Create", JOptionPane.WARNING_MESSAGE);
+				return;
+			}
+			//We have no errors time to make a human
+			Human construction = new Human();
+			construction.name = name.getText();
+			construction.owner = owner.getText();
+			construction.xp = Integer.parseInt(exp.getText());
+			construction.aim = Integer.parseInt(aim.getText());
+			construction.will = Integer.parseInt(will.getText());
+			construction.health = Byte.parseByte(health.getText());
+			construction.movement = Byte.parseByte(movement.getText());
+			construction.charClass = (byte) className.getSelectedIndex();
+			construction.nationality = (byte) nations.getSelectedIndex();
+			construction.mec = mech.isSelected();
+			construction.gene = gene.isSelected();
+			construction.psi = psi.isSelected();
+			construction.sex = male.isSelected();
+			humansList.add(construction);
+			saveHuman.setEnabled(true);
+			deleteHuman.setEnabled(true);
+			addHuman.setEnabled(false);
+			humans.addItem(construction.name);
+			humans.setSelectedItem(construction.name);
+			changes = true;
+			expLabel.setText("XP Total: " + construction.xp);
+			rankLabel.setText("Rank: " + construction.getRank());
+		}
+		else if(arg0.getSource() == deleteHuman){
+			int ret = JOptionPane.showConfirmDialog(this, "Are you sure you want to delete this human?");
+			if(ret != 0)
+				return;
+			humansList.remove(humeIndex);
+			humans.removeItemAt(humeIndex+1);
+			humans.setSelectedIndex(0);
+		}
+		else if(arg0.getSource() == humans){
+			
+			if(humans.getSelectedItem() == null){
+				
+			}
+			else if(((String)humans.getSelectedItem()).equals("<New Human>")){
+				//Adding a new Human
+				saveHuman.setEnabled(false);
+				deleteHuman.setEnabled(false);
+				addHuman.setEnabled(true);
+				name.setText("");
+				name.setEnabled(true);
+				className.setSelectedIndex(0);
+				className.setEnabled(true);
+				male.setEnabled(true);
+				male.setSelected(false);
+				
+				female.setEnabled(true);
+				female.setSelected(false);
+				owner.setText("");
+				owner.setEnabled(true);
+				nations.setSelectedIndex(0);
+				nations.setEnabled(true);
+				health.setText("");
+				aim.setText("");
+				will.setText("");
+				movement.setText("");
+				mech.setEnabled(true);
+				mech.setSelected(false);
+				gene.setEnabled(true);
+				gene.setSelected(false);
+				norm.setEnabled(true);
+				norm.setSelected(false);
+				
+				psi.setSelected(false);
+				notPsi.setSelected(false);
+				exp.setText("");
+				rankLabel.setText("Rank: ");
+				abilitiesLabel.setText("Abilities: ");
+				medals.setText("Medals: ");
+				expLabel.setText("XP Total: ");
+				humeIndex = 0;
+			}
+			else{
+				//Editing an existing human
+				saveHuman.setEnabled(true);
+				deleteHuman.setEnabled(true);
+				addHuman.setEnabled(false);
+				humeIndex = 0;
+				for(int i = 0; i < humansList.size(); i++){
+					if(humansList.get(i).name.equals(humans.getSelectedItem())){
+						humeIndex = i;
+						break;
+					}
+				}
+				Human chosen = humansList.get(humeIndex);
+				name.setText(chosen.name);
+				name.setEnabled(false);
+				className.setSelectedIndex(chosen.charClass);
+				if(chosen.xp >= 90)
+					className.setEnabled(false);
+				else
+					className.setEnabled(true);
+				male.setSelected(chosen.sex);
+				male.setEnabled(false);
+				female.setSelected(!chosen.sex);
+				male.setEnabled(false);
+				female.setEnabled(false);
+				owner.setText(chosen.owner);
+				owner.setEnabled(false);
+				nations.setSelectedIndex(chosen.nationality);
+				nations.setEnabled(false);
+				health.setText(String.valueOf(chosen.health));
+				aim.setText(String.valueOf(chosen.aim));
+				will.setText(String.valueOf(chosen.will));
+				movement.setText(String.valueOf(chosen.movement));
+				mech.setSelected(chosen.mec);
+				gene.setSelected(chosen.gene);
+				norm.setSelected(!chosen.mec && ! chosen.gene);
+				if(!norm.isSelected()){
+					mech.setEnabled(false);
+					gene.setEnabled(false);
+					norm.setEnabled(false);
+				}
+				else{
+					mech.setEnabled(true);
+					gene.setEnabled(true);
+					norm.setEnabled(true);
+				}
+				psi.setSelected(chosen.psi);
+				notPsi.setSelected(!chosen.psi);
+				exp.setText("");
+				rankLabel.setText("Rank: " + chosen.getRank());
+				abilitiesLabel.setText("<html>Abilities: " + chosen.getAbilitiesString());
+				medals.setText("Medals: " + chosen.getMedalsString());
+				expLabel.setText("XP Total: " + chosen.xp);
+			}
+		}
+		
+		
 	}
 
 	@Override
@@ -279,32 +642,75 @@ public class PlayerGUI extends GUITemplate implements MouseListener {
 
 	@Override
 	public void mouseClicked(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
+		if(arg0.getClickCount() == 2){
+			if(arg0.getSource() == abilitiesLabel && !name.isEnabled()){
+				//Edit abilities list
+				
+				Human chosen = humansList.get(humeIndex);
+				String[] choices = chosen.getPossibleAbilities();
+				String ability = (String) JOptionPane.showInputDialog(this, "Chose an abilty to add or remove", "Ability Add/Remove", JOptionPane.PLAIN_MESSAGE, null, choices, choices[0]);
+				if(ability == null)
+					return;
+				if(chosen.hasAbility(ability)){
+					chosen.removeAbility(ability);
+				}
+				else{
+					chosen.addAbility(ability);
+				}
+				String temp = "<html>Abilities: " + chosen.getAbilitiesString();
+				abilitiesLabel.setText(temp);
+			}
+			else if(arg0.getSource() == medals && !name.isEnabled()){
+				Human chosen = humansList.get(humeIndex);
+				String medal = (String) JOptionPane.showInputDialog(this, "Chose an abilty to add or remove", "Ability Add/Remove", JOptionPane.PLAIN_MESSAGE, null, Human.medals, Human.medals[0]);
+				if(medal == null)
+					return;
+				if(chosen.hasMedal(medal)){
+					chosen.removeMedal(medal);
+				}
+				else{
+					chosen.addMedal(medal);
+				}
+				medals.setText("Medals: " + chosen.getMedalsString());
+			}
+		}
 	}
 
 	@Override
 	public void mouseEntered(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void mouseExited(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void mousePressed(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent arg0) {
-		// TODO Auto-generated method stub
-		
+
 	}
 
+	@Override
+	public void dispose(){
+		if(!changes){
+			Core.guis.remove(this);
+			super.dispose();
+			return;
+		}
+		int ret = JOptionPane.showConfirmDialog(this, "Do you want to save the changes to this item?");
+		if(ret == 1){
+			Core.guis.remove(this);
+			super.dispose();
+			return;
+		}
+		if(ret == 2)
+			return;
+		saveDialog();
+		Core.guis.remove(this);
+		super.dispose();
+	}
+	
 }
